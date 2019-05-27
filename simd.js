89d604ccd7d8a981d891adfa6beef5f875e24e27
Alexander Teslovskiy <artoriousso@gmail.com>
2019-05-28 00:02:05 +0300

    Add all values, refactor

--- simd.js
+++ simd.js
@@ -18,11 +18,14 @@ $(document).ready(() => {
     subtractionTime = 1,
     comparisonTime = 1
 
+  setDefaults()
   generate()
   calculate()
   draw()
 
   $('#run-button').on('click', () => {
+    if (!validate()) return
+
     sizeP = $('#p-size').val()
     sizeM = $('#m-size').val()
     sizeQ = $('#q-size').val()
@@ -33,15 +36,32 @@ $(document).ready(() => {
     subtractionTime = $('#subtraction-time').val()
     comparisonTime = $('#comparison-time').val()
 
-    if (!sizeP || !sizeM || !sizeQ) {
-      alert('Input all sizes')
-      return
-    }
-
     generate()
     calculate()
     draw()
   })
+
+  function validate() {
+    if ($('#p-size').val() === '' || $('#m-size').val() === '' || $('#q-size').val() === '' ||
+    $('#n-value').val() === '' || $('#multiplication-time').val() === '' || $('#division-time').val() === '' ||
+    $('#addition-time').val() === '' || $('#subtraction-time').val() === '' || $('#comparison-time').val() === '') {
+      alert('Data is incorrect')
+      return false
+    }
+    return true
+  }
+
+  function setDefaults() {
+    $('#p-size').val('3')
+    $('#m-size').val('3')
+    $('#q-size').val('3')
+    $('#n-value').val('1')
+    $('#multiplication-time').val('1')
+    $('#division-time').val('1')
+    $('#addition-time').val('1')
+    $('#subtraction-time').val('1')
+    $('#comparison-time').val('1')
+  }
 
   function generate() {
     a = generateMatrix(sizeP, sizeM)
@@ -80,6 +100,11 @@ $(document).ready(() => {
   }
 
   function draw() {
+    let rang = sizeM * sizeP * sizeQ
+    let consistentTime = numberOfMultiplications() * multiplicationTime + numberOfAdditions() * additionTime +
+      numberOfSubtractions() * subtractionTime + numberOfComparisons() * comparisonTime
+    let parallelTime = consistentTime / nValue
+
     $('.matrices').children('table').each((_, table) => {
       $(table).empty()
     })
@@ -90,16 +115,19 @@ $(document).ready(() => {
     drawMatrix(e, $('#e-matrix'))
     drawMatrix(g, $('#g-matrix'))
     drawMatrix(c, $('#c-matrix'))
+
     $('#result-multiplication-time').text(numberOfMultiplications() * multiplicationTime)
     $('#result-addition-time').text(numberOfAdditions() * additionTime)
     $('#result-subtraction-time').text(numberOfSubtractions() * subtractionTime)
     $('#result-comparison-time').text(numberOfComparisons() * comparisonTime)
-    $('#result-rang').text(sizeM * sizeP * sizeQ)
-    let fullTime = numberOfMultiplications() * multiplicationTime + numberOfAdditions() * additionTime +
-      numberOfSubtractions() * subtractionTime + numberOfComparisons() * comparisonTime
-    $('#result-consistent-time').text(fullTime)
-    $('#result-parallel-time').text()
-
+    $('#result-acceleration-factor').text((consistentTime / parallelTime).toFixed(3))
+    $('#result-efficiency').text((consistentTime / parallelTime / nValue).toFixed(3))
+    $('#result-rang').text(rang)
+    $('#result-parallel-time').text(parallelTime)
+    $('#result-consistent-time').text(consistentTime)
+    $('#result-total-length').text(parallelTime)
+    $('#result-average-length').text(Math.ceil(parallelTime / rang))
+    $('#result-discrepancy').text(rang)
   }
 
   function drawMatrix(matrix, table) {
@@ -114,7 +142,7 @@ $(document).ready(() => {
   }
 
   // a -> b
-  function implication(first, second) {
+  function reduction(first, second) {
     return Math.max(1 - first, second)
   }
 
@@ -153,13 +181,13 @@ $(document).ready(() => {
   }
 
   function calculateF() {
-    for (var i = 0; i < sizeQ; i++) {
-      for (var j = 0; j < sizeP; j++) {
+    for (var i = 0; i < sizeP; i++) {
+      for (var j = 0; j < sizeQ; j++) {
         for (var k = 0; k < sizeM; k++) {
           f[i][j][k] = (
-            implication(a[i][k], b[k][j]) * (2 * e[0][k] - 1) * e[0][k] +
-            implication(b[k][j], a[i][k]) * (
-              1 + (4 * implication(a[i][k], b[k][j]) - 2) * e[0][k]
+            reduction(a[i][k], b[k][j]) * (2 * e[0][k] - 1) * e[0][k] +
+            reduction(b[k][j], a[i][k]) * (
+              1 + (4 * reduction(a[i][k], b[k][j]) - 2) * e[0][k]
             ) * (1 - e[0][k])
           ).toFixed(3)
         }
@@ -168,8 +196,8 @@ $(document).ready(() => {
   }
 
   function calculateD() {
-    for (var i = 0; i < sizeQ; i++) {
-      for (var j = 0; j < sizeP; j++) {
+    for (var i = 0; i < sizeP; i++) {
+      for (var j = 0; j < sizeQ; j++) {
         for (var k = 0; k < sizeM; k++) {
           d[i][j][k] = conjunction(a[i][k], b[k][j]).toFixed(3)
         }
@@ -178,8 +206,8 @@ $(document).ready(() => {
   }
 
   function calculateC() {
-    for (var i = 0; i < sizeQ; i++) {
-      for (var j = 0; j < sizeP; j++) {
+    for (var i = 0; i < sizeP; i++) {
+      for (var j = 0; j < sizeQ; j++) {
         c[i][j] = (
           singleConjunction(f[i][j]) * (3 * g[i][j] - 2) * g[i][j] + (
             singleDisjunction(d[i][j]) + (4 * (multiplication(f[i][j], d[i][j]) - 3 *
@@ -206,3 +234,9 @@ $(document).ready(() => {
     return (12 + sizeM) * sizeP * sizeQ
   }
 })
+
+// Tn = tMultiplicity * Math.ceil(p * q * m / n)
+// Lsum = Tn
+// Lavg = T1 / r
+// D = Lsum / Lavg
+// parallelTime = (consistentTime - (sizeM * 2 * multiplicationTime) - (sizeM * 3 * multiplicationTime + subtractionTime) - subtractionTime)
