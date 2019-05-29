35d28b13b7eeba40ab71663508ed7ba03d4b379f
Alexander Teslovskiy <artoriousso@gmail.com>
2019-05-29 18:57:59 +0300

    Fix time calculation, add graphics

--- simd.js
+++ simd.js
@@ -13,13 +13,25 @@ $(document).ready(() => {
     sizeM = 3,
     sizeQ = 3,
     nValue = 1,
+    rang = 9,
     multiplicationTime = 1,
     additionTime = 1,
     subtractionTime = 1,
-    comparisonTime = 1
+    comparisonTime = 1,
+    numberOfMultiplications = 0,
+    numberOfAdditions = 0,
+    numberOfSubtractions = 0,
+    numberOfComparisons = 0,
+    numberOfParallelMultiplications = 0,
+    numberOfParallelAdditions = 0,
+    numberOfParallelSubtractions = 0,
+    consistentTime = 0,
+    parallelTime = 0
+
+  google.charts.load('current', {packages: ['corechart', 'line']})
+  google.charts.setOnLoadCallback(drawGraphics)
 
   setDefaults()
-  generate()
   calculate()
   draw()
 
@@ -36,7 +48,6 @@ $(document).ready(() => {
     subtractionTime = $('#subtraction-time').val()
     comparisonTime = $('#comparison-time').val()
 
-    generate()
     calculate()
     draw()
   })
@@ -100,11 +111,6 @@ $(document).ready(() => {
   }
 
   function draw() {
-    let rang = sizeM * sizeP * sizeQ
-    let consistentTime = numberOfMultiplications() * multiplicationTime + numberOfAdditions() * additionTime +
-      numberOfSubtractions() * subtractionTime + numberOfComparisons() * comparisonTime
-    let parallelTime = consistentTime / nValue
-
     $('.matrices').children('table').each((_, table) => {
       $(table).empty()
     })
@@ -116,12 +122,12 @@ $(document).ready(() => {
     drawMatrix(g, $('#g-matrix'))
     drawMatrix(c, $('#c-matrix'))
 
-    $('#result-multiplication-time').text(numberOfMultiplications() * multiplicationTime)
-    $('#result-addition-time').text(numberOfAdditions() * additionTime)
-    $('#result-subtraction-time').text(numberOfSubtractions() * subtractionTime)
-    $('#result-comparison-time').text(numberOfComparisons() * comparisonTime)
-    $('#result-acceleration-factor').text((consistentTime / parallelTime).toFixed(3))
-    $('#result-efficiency').text((consistentTime / parallelTime / nValue).toFixed(3))
+    $('#result-multiplication-time').text(numberOfMultiplications * multiplicationTime)
+    $('#result-addition-time').text(numberOfAdditions * additionTime)
+    $('#result-subtraction-time').text(numberOfSubtractions * subtractionTime)
+    $('#result-comparison-time').text(numberOfComparisons * comparisonTime)
+    $('#result-acceleration-factor').text(accelerationFactor)
+    $('#result-efficiency').text(efficiency)
     $('#result-rang').text(rang)
     $('#result-parallel-time').text(parallelTime)
     $('#result-consistent-time').text(consistentTime)
@@ -143,11 +149,14 @@ $(document).ready(() => {
 
   // a -> b
   function reduction(first, second) {
+    numberOfSubtractions++
+    numberOfComparisons += 2
     return Math.max(1 - first, second)
   }
 
   // a ^ b
   function conjunction(first, second) {
+    numberOfComparisons += 2
     return Math.min(first, second)
   }
 
@@ -156,7 +165,9 @@ $(document).ready(() => {
     var value = 1
     for (var i = 0; i < array.length; i++) {
       value *= array[i]
+      numberOfMultiplications++
     }
+    numberOfMultiplications--
     return value
   }
 
@@ -165,19 +176,53 @@ $(document).ready(() => {
     var value = 1
     for (var i = 0; i < array.length; i++) {
       value *= (1 - array[i])
+      numberOfMultiplications++
+      numberOfSubtractions++
     }
+    numberOfMultiplications--
+    numberOfSubtractions++
     return 1 - value
   }
 
   // a o b
   function multiplication(first, second) {
+    numberOfAdditions++
+    numberOfSubtractions++
+    numberOfComparisons += 2
     return Math.max(singleConjunction(first) + singleDisjunction(second) - 1, 0)
   }
 
-  function calculate() {
+  function calculate(m, p, q, n) {
+    if (m && p && q && n) {
+      sizeM = m
+      sizeP = p
+      sizeQ = q
+      nValue = n
+    }
+
+    numberOfMultiplications = 0
+    numberOfAdditions = 0
+    numberOfSubtractions = 0
+    numberOfComparisons = 0
+    numberOfParallelMultiplications = 0
+    numberOfParallelAdditions = 0
+    numberOfParallelSubtractions = 0
+
+    generate()
     calculateF()
     calculateD()
     calculateC()
+
+    rang = sizeM * sizeP * sizeQ
+    consistentTime = numberOfMultiplications * multiplicationTime + numberOfAdditions * additionTime +
+      numberOfSubtractions * subtractionTime + numberOfComparisons * comparisonTime + numberOfParallelMultiplications *
+      multiplicationTime + numberOfParallelAdditions * additionTime + numberOfParallelSubtractions * subtractionTime
+    parallelTime = numberOfMultiplications * multiplicationTime + numberOfAdditions * additionTime +
+      numberOfSubtractions * subtractionTime + numberOfComparisons * comparisonTime + (numberOfParallelMultiplications
+      * multiplicationTime + numberOfParallelAdditions * additionTime + numberOfParallelSubtractions * subtractionTime)
+      / nValue
+    accelerationFactor = consistentTime / parallelTime
+    efficiency = consistentTime / (parallelTime * nValue)
   }
 
   function calculateF() {
@@ -190,6 +235,9 @@ $(document).ready(() => {
               1 + (4 * reduction(a[i][k], b[k][j]) - 2) * e[0][k]
             ) * (1 - e[0][k])
           ).toFixed(3)
+          numberOfParallelMultiplications += 7
+          numberOfParallelAdditions += 2
+          numberOfParallelSubtractions += 3
         }
       }
     }
@@ -214,29 +262,94 @@ $(document).ready(() => {
             singleDisjunction(d[i][j])) * g[i][j])
           ) * (1 - g[i][j])
         ).toFixed(3)
+        numberOfParallelMultiplications += 6
+        numberOfParallelAdditions += 2
+        numberOfParallelSubtractions += 2
       }
     }
   }
 
-  function numberOfMultiplications() {
-    return (13 + 3 * sizeM) * sizeP * sizeQ
+  function drawGraphics() {
+    let KyN1 = [], KyN2 = [], KyN3 = [], KyN4 = [], KyR1 = [], KyR2 = [], KyR3 = [], KyR4 = []
+    let En1 = [], En2 = [], En3 = [], En4 = [], Er1 = [], Er2 = [], Er3 = [], Er4 = []
+    let Dn1 = [], Dn2 = [], Dn3 = [], Dn4 = [], Dr1 = [], Dr2 = [], Dr3 = [], Dr4 = []
+    let KyN = [], En = [], Dn = [], KyR = [], Er = [], Dr = []
+
+    for (var i = 1; i <= 50; i++) {
+      calculate(1, 1, 1, i)
+      KyN1.push(accelerationFactor)
+      En1.push(efficiency)
+      Dn1.push(rang)
+      calculate(2, 1, 1, i)
+      KyN2.push(accelerationFactor)
+      En2.push(efficiency)
+      Dn2.push(rang)
+      calculate(3, 1, 1, i)
+      KyN3.push(accelerationFactor)
+      En3.push(efficiency)
+      Dn3.push(rang)
+      calculate(4, 1, 1, i)
+      KyN4.push(accelerationFactor)
+      En4.push(efficiency)
+      Dn4.push(rang)
+
+      KyN.push([i, KyN1[i - 1], KyN2[i - 1], KyN3[i - 1], KyN4[i - 1]])
+      En.push([i, En1[i - 1], En2[i - 1], En3[i - 1], En4[i - 1]])
+      Dn.push([i, Dn1[i - 1], Dn2[i - 1], Dn3[i - 1], Dn4[i - 1]])
+
+      calculate(i, 1, 1, 1)
+      KyR1.push(accelerationFactor)
+      Er1.push(efficiency)
+      Dr1.push(rang)
+      calculate(i, 1, 1, 2)
+      KyR2.push(accelerationFactor)
+      Er2.push(efficiency)
+      Dr2.push(rang)
+      calculate(i, 1, 1, 3)
+      KyR3.push(accelerationFactor)
+      Er3.push(efficiency)
+      Dr3.push(rang)
+      calculate(i, 1, 1, 4)
+      KyR4.push(accelerationFactor)
+      Er4.push(efficiency)
+      Dr4.push(rang)
+
+      KyR.push([i, KyR1[i - 1], KyR2[i - 1], KyR3[i - 1], KyR4[i - 1]])
+      Er.push([i, Er1[i - 1], Er2[i - 1], Er3[i - 1], Er4[i - 1]])
+      Dr.push([i, Dr1[i - 1], Dr2[i - 1], Dr3[i - 1], Dr4[i - 1]])
+    }
+
+    drawGraphic("1", "2", "3", "4", KyN, "n", "Ky", "ku-n")
+    drawGraphic("1", "2", "3", "4", En, "n", "e", "e-n")
+    drawGraphic("1", "2", "3", "4", Dn, "n", "D", "d-n")
+    drawGraphic("1", "2", "3", "4", KyR, "r", "Ky", "ku-r")
+    drawGraphic("1", "2", "3", "4", Er, "r", "e", "e-r")
+    drawGraphic("1", "2", "3", "4", Dr, "r", "D", "d-r")
   }
 
-  function numberOfAdditions() {
-    return 6 * sizeP * sizeQ
-  }
+  function drawGraphic(name1, name2, name3, name4, rows, horizontalTitle, verticalTitle, elementId) {
+    var data = new google.visualization.DataTable()
+    data.addColumn('number', 'X')
+    data.addColumn('number', name1)
+    data.addColumn('number', name2)
+    data.addColumn('number', name3)
+    data.addColumn('number', name4)
+    data.addRows(rows)
 
-  function numberOfSubtractions() {
-    return (9 + sizeM) * sizeP * sizeQ
-  }
+    var options = {
+      hAxis: {
+        title: horizontalTitle
+      },
+      vAxis: {
+        title: verticalTitle
+      },
+    }
 
-  function numberOfComparisons() {
-    return (12 + sizeM) * sizeP * sizeQ
+    var chart = new google.visualization.LineChart(document.getElementById(elementId))
+    chart.draw(data, options)
   }
 })
 
-// Tn = tMultiplicity * Math.ceil(p * q * m / n)
 // Lsum = Tn
 // Lavg = T1 / r
 // D = Lsum / Lavg
-// parallelTime = (consistentTime - (sizeM * 2 * multiplicationTime) - (sizeM * 3 * multiplicationTime + subtractionTime) - subtractionTime)
